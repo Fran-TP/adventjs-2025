4b82238f39c3d76e6d3ca5ca08073d56de22e762
Fran Torres <ftorresp@unasam.edu.pe>
2025-12-09 02:22:20 -0500

    Refactor findUniqueToy logic and add tests
    Note: correct results, but bad logic

--- challenge-08/find-the-unique-toy.ts
+++ challenge-08/find-the-unique-toy.ts
@@ -1,38 +1,30 @@
-export default function findUniqueToy(toy: string): string {
+export default function findUniqueToy(toy: string) {
 	const seen = new Set<string>()
+	const history = new Set<string>()
 
-	let start = 0
-	let end = toy.length - 1
-
-	for (;;) {
-		if (end <= start) {
-			// console.log({ start, end })
-			break
-		}
-
-		// console.log({ start, end })
-
-		if (seen.has(toy[start]?.toLocaleLowerCase() ?? '')) {
-			start++
-
-			continue
-		}
-
-		seen.add(toy[end]?.toLocaleLowerCase() ?? '')
-
-		if (toy[start]?.toLocaleLowerCase() === toy[end]?.toLocaleLowerCase()) {
-			start++
-			end--
+	for (const char of toy) {
+		if (
+			!seen.delete(char.toUpperCase()) &&
+			!seen.delete(char.toLowerCase()) &&
+			!history.has(char.toLowerCase())
+		) {
+			seen.add(char)
 		} else {
-			end--
+			history.add(char.toLowerCase())
 		}
 	}
 
-	return toy[start] === toy[end + 1] ? '*' : (toy[start] ?? '')
+	const { value } = seen[Symbol.iterator]().next()
+
+	return value ?? ''
 }
 
 console.log(findUniqueToy('sotarsof')) // result -> t
-console.log(findUniqueToy('Gift')) // result -> g
+console.log(findUniqueToy('Gift')) // result -> G
 console.log(findUniqueToy('sS')) // result -> ''
 console.log(findUniqueToy('reindeeR')) // result -> i
 console.log(findUniqueToy('sarstAN')) // result -> r
+console.log(findUniqueToy('Sss')) // ''
+console.log(findUniqueToy('sSsa')) // a
+console.log(findUniqueToy('ss')) // ''
+console.log(findUniqueToy('')) // ''
