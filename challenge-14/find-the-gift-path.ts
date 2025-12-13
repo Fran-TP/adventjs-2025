9c12076965a3b612684d6fc29dd55be050a21aa3
Fran Torres <ftorresp@unasam.edu.pe>
2025-12-13 10:41:38 -0500

    Remove example usage from find-the-gift-path

--- challenge-14/find-the-gift-path.ts
+++ challenge-14/find-the-gift-path.ts
@@ -21,29 +21,3 @@ export default function findGiftPath(workshop: Workshop, gift: Gift): Path {
 
 	return []
 }
-
-const workshop = {
-	storage: {
-		shelf: {
-			box1: 'train',
-			box2: 'switch'
-		},
-		box: 'car'
-	},
-	gift: 'doll'
-}
-
-console.log(findGiftPath(workshop, 'train'))
-// ➜ ['storage', 'shelf', 'box1']
-
-// console.log(findGiftPath(workshop, 'switch'))
-// ➜ ['storage', 'shelf', 'box2']
-
-// console.log(findGiftPath(workshop, 'car'))
-// ➜ ['storage', 'box']
-
-// console.log(findGiftPath(workshop, 'doll'))
-// ➜ ['gift']
-
-// console.log(findGiftPath(workshop, 'plane'))
-// ➜ []
