8b583d07031270a401dd20183c78090dffe82683
Fran Torres <ftorresp@unasam.edu.pe>
2025-12-16 00:16:56 -0500

    Refactor drawTable types, sorting, and layout

--- challenge-15/drawing-tables.ts
+++ challenge-15/drawing-tables.ts
@@ -1,45 +1,45 @@
-type Data = Record<string, string | number>[]
-type SortBy = string
+export default function drawTable<T extends Record<string, string | number>>(
+	data: T[],
+	sortBy: keyof T
+): string {
+	const widths: Record<string, number> = {}
+	const headers = Object.keys(data[0]!)
 
-export default function drawTable(data: Data, sortBy: SortBy): string {
-	const sizes: Record<string, number> = {}
-	const sortedData = data.toSorted((a, b) =>
-		typeof a[sortBy] === 'string'
-			? String(a[sortBy]).localeCompare(String(b[sortBy]))
-			: Number(a[sortBy]) - Number(b[sortBy])
-	)
 	let body = ''
 
-	for (const item of sortedData) {
-		for (const key in item) {
-			const value =
-				typeof item[key] === 'string'
-					? item[key].length
-					: String(item[key]).length
+	for (const row of data) {
+		for (const key in row) {
+			const value = String(row[key]).length
 
-			sizes[key] ??= value
-			sizes[key] = Math.max(sizes[key], value)
+			widths[key] ??= value
+			widths[key] = Math.max(widths[key], value)
 		}
 	}
 
+	const sortedData = data.toSorted((a, b) => {
+		const va = a[sortBy]
+		const vb = b[sortBy]
+
+		return typeof va === 'string'
+			? va.localeCompare(String(vb))
+			: Number(va) - Number(vb)
+	})
+
 	let divider = '+'
 	let header = '|'
-	let firstTitle = 65
 
-	for (const key in sizes) {
-		const size = Number(sizes[key])
+	for (const [i, head] of headers.entries()) {
+		const width = Number(widths[head])
 
-		divider += `${'-'.repeat(size + 2)}+`
-		header += ` ${String.fromCharCode(firstTitle).padEnd(size)} |`
-
-		firstTitle++
+		divider += `${'-'.repeat(width + 2)}+`
+		header += ` ${String.fromCharCode(65 + i).padEnd(width)} |`
 	}
 
 	for (const item of sortedData) {
 		let row = '|'
-		for (const key in sizes) {
+		for (const key in widths) {
 			const val = String(item[key])
-			const size = Number(sizes[key])
+			const size = Number(widths[key])
 
 			row += ` ${val.padEnd(size)} |`
 		}
@@ -49,24 +49,3 @@ export default function drawTable(data: Data, sortBy: SortBy): string {
 
 	return `${divider}\n${header}\n${divider}\n${body}${divider}`
 }
-
-console.log(
-	drawTable(
-		[
-			{ name: 'Charlie', city: 'New York' },
-			{ name: 'Alice', city: 'London' },
-			{ name: 'Bob', city: 'Paris' }
-		],
-		'name'
-	)
-)
-console.log(
-	drawTable(
-		[
-			{ gift: 'Book', quantity: 5 },
-			{ gift: 'Music CD', quantity: 1 },
-			{ gift: 'Doll', quantity: 10 }
-		],
-		'quantity'
-	)
-)
