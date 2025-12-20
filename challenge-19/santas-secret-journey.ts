2bc770e29b524faa61c5b3afff900b45b064dbd7
Fran Torres <ftorresp@unasam.edu.pe>
2025-12-19 20:43:00 -0500

    Implement revealSantaRoute for challenge 19

---
+++ challenge-19/santas-secret-journey.ts
@@ -0,0 +1,45 @@
+export default function revealSantaRoute(routes: [string, string][]): string[] {
+	const paths = new Map(routes)
+	let path = routes[0]![0]!
+
+	const resultPath: string[] = []
+
+	while (true) {
+		if (paths.has(path)) {
+			resultPath.push(path)
+
+			path = paths.get(path)!
+		} else {
+			resultPath.push(path)
+
+			return resultPath
+		}
+	}
+}
+
+console.log(
+	revealSantaRoute([
+		['USA', 'BRA'],
+		['JPN', 'PHL'],
+		['BRA', 'UAE'],
+		['UAE', 'JPN'],
+		['CMX', 'HKN']
+	])
+) // → ['USA', 'BRA', 'UAE', 'JPN', 'PHL']
+
+console.log(
+	revealSantaRoute([
+		['MEX', 'CAN'],
+		['UK', 'GER'],
+		['CAN', 'UK']
+	])
+)
+// → ['MEX', 'CAN', 'UK', 'GER']
+
+console.log(
+	revealSantaRoute([
+		['STA', 'HYD'],
+		['ESP', 'CHN']
+	])
+)
+// → ['STA', 'HYD']
